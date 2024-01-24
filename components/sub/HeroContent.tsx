4ebcee5f71a423eed63adff250d77b6f26d231b6
sanjanabharath <sanjanabarath@gmail.com>
2024-01-24 21:40:47 +0530

    added registeration

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -6,7 +6,9 @@ import { slideInFromLeft, slideInFromRight } from "@/utils/motion";
 import Countdown from "../main/Countdown";
 import EventStarts from "../main/EventStarts";
 import mainLogo from "../assets/main.png";
+import DailogBox from "../sub/DailogBox";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const HeroContent = () => {
   const launchDate = new Date("2024-02-17T00:00:00Z");
@@ -15,12 +17,12 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center mt-40 w-full z-[20]"
+      className="flex flex-row items-center mt-20 w-full z-[20]"
     >
       <div className="flex flex-row items-center text-center">
         <motion.div
-          variants={slideInFromLeft(0.8)}
-          className="w-full h-full flex justify-center items-center"
+          variants={slideInFromLeft(1)}
+          className="w-full h-full flex justify-items-start items-center"
         >
           <Image src={mainLogo} alt="work icons" height={650} width={650} />
         </motion.div>
@@ -33,7 +35,7 @@ const HeroContent = () => {
             variants={slideInFromLeft(0.5)}
             className="gap-6 mt-3 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
           >
-            <span>
+            <span style={{ font: "" }}>
               Xplo
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
                 IT
@@ -42,8 +44,20 @@ const HeroContent = () => {
             </span>
           </motion.div>
 
-          <EventStarts launchDate={launchDate} />
           <Countdown />
+          <motion.div
+            variants={slideInFromLeft(0.5)}
+            className=" mt-3 font-bold text-white w-auto h-auto"
+          >
+            <DailogBox />
+            <motion.a
+              variants={slideInFromLeft(1)}
+              className="p-3 button-primary text-center text-white cursor-pointer rounded-lg max-w-[400px] mx-3"
+              href="#events"
+            >
+              Explore More!
+            </motion.a>
+          </motion.div>
         </motion.p>
       </div>
     </motion.div>
