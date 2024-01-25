0a4a9e796120c6121268c06e348eb240cbd5438b
sanjanabharath <sanjanabarath@gmail.com>
2024-01-25 23:18:47 +0530

    registration customized

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import college from "../assets/veclogo.png";
 
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0  backdrop-blur-md z-[50] px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[5px]">
         <a href="#home" className="h-auto w-auto flex flex-row items-center">
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            #XploITs2024
+          <span className="flex justify-center">
+            <img
+              src="/veclogo.png"
+              alt="college logo"
+              className="w-[700px] mt-20"
+            />
           </span>
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300"></span>
           <div className="flex items-center justify-between w-full h-auto border-0 bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#home" className="cursor-pointer">
               Home
